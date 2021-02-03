4775a3fc56440a0cc0d1b748fe818718a3ef4119
João Nabais <joaonabais@alentapp.pt>
2021-02-03 00:10:49 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+<!-- VARS -->
+
+[star-badge]: https://img.shields.io/github/stars/joaomnabais/react-native-authentication?logo=github
+[last-commit-badge]: https://img.shields.io/github/last-commit/joaomnabais/react-native-authentication
+[issues-url]: https://github.com/joaomnabais/react-native-authentication/issues/
+[node-url]: https://nodejs.org/en
+[yarn-url]: https://classic.yarnpkg.com/
+[npm-url]:  https://www.npmjs.com/
+[expo-url]: https://expo.io/
+
+<!-- VARS -->
+
+<p align="center">
+  React Native authentication using Context API and Typescript 🚀
+</p>
+
+
+
+<div align="center">  
+
+![Git Stars][star-badge]
+![Last Commit][last-commit-badge]
+
+
+</div>
+
+
+### Content
+* [Getting Started](#Getting-Started-)
+    * [Cloning](#Cloning)
+    * [Requirements](#Requirements)
+    * [Running](#Running)
+* [Issues](#Issues-)
+    * [Report](#Report)
+
+### Getting Started 🚀
+
+#### Cloning
+
+```ps
+# Create a directory in your desired location
+# Clone the repository inside the directory using git
+$ git clone https://github.com/joaomnabais/react-native-authentication.git
+```
+
+#### Requirements
+* [Node.js][node-url]
+* [Yarn][yarn-url] or [npm][npm-url]
+* [Expo][expo-url]
+
+This project use third party dependencies that need to be installed, use that command to install all needed dependencies
+
+```ps
+$ yarn install
+```
+
+>The above command will install all third party dependencies used.
+
+#### Running
+To start the project run the command
+
+```ps
+$ expo start
+```
+
+### Issues 🐛
+
+#### Report
+
+In case you are having any problem you can report in [Issues][issues-url] session.
---
+++ src/contexts/auth.tsx
@@ -0,0 +1,72 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import * as auth from '../services/auth';
+import api from '../services/api';
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface AuthContextData {
+  signed: boolean;
+  user: User | null;
+  loading: boolean;
+  signIn(): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadStorageData() {
+      const storageUser = await AsyncStorage.getItem('@RNAuth:user');
+      const storageToken = await AsyncStorage.getItem('@RNAuth:token');
+
+      if (storageUser && storageToken) {
+        api.defaults.headers['Authorization'] = `Bearer ${storageToken}`;
+
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      } else if (!storageUser) {
+        setLoading(false);
+      }
+    }
+
+    loadStorageData();
+  }, []);
+
+  async function signIn() {
+    const response = await auth.signIn();
+
+    const { token, user } = response;
+
+    setUser(response.user);
+
+    api.defaults.headers['Authorization'] = `Bearer ${response.token}`;
+
+    await AsyncStorage.setItem('@RNAuth:user', JSON.stringify(response.user));
+    await AsyncStorage.setItem('@RNAuth:token', response.token);
+  }
+
+  async function signOut() {
+    await AsyncStorage.clear();
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider value={{ signed: !!user, user, loading, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+};
